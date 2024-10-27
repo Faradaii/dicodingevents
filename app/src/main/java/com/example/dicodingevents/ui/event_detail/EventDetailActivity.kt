5a12b15ba2196752824076da69f06d3a414ead3b
Faradaii <kdkfrdy@gmail.com>
2024-10-27 21:04:10 +0800

    fix: button bug, notification intent dark mode bug, loading state

--- app/src/main/java/com/example/dicodingevents/ui/event_detail/EventDetailActivity.kt
+++ app/src/main/java/com/example/dicodingevents/ui/event_detail/EventDetailActivity.kt
@@ -1,15 +1,33 @@
 package com.example.dicodingevents.ui.event_detail
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.lifecycleScope
 import com.example.dicodingevents.R
 import com.example.dicodingevents.databinding.ActivityEventDetailBinding
+import com.example.dicodingevents.ui.MainNavigation
+import com.example.dicodingevents.utils.SettingPreferences
+import com.example.dicodingevents.utils.dataStore
+import kotlinx.coroutines.launch
 
 class EventDetailActivity : AppCompatActivity() {
     private lateinit var activityEventDetailBinding: ActivityEventDetailBinding
+    private lateinit var settingPreferences: SettingPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        settingPreferences = SettingPreferences.getInstance(dataStore)
+
+        lifecycleScope.launch {
+            settingPreferences.getThemeSetting().collect { isDarkModeActive ->
+                AppCompatDelegate.setDefaultNightMode(
+                    if (isDarkModeActive) AppCompatDelegate.MODE_NIGHT_YES
+                    else AppCompatDelegate.MODE_NIGHT_NO
+                )
+            }
+        }
 
         activityEventDetailBinding = ActivityEventDetailBinding.inflate(layoutInflater)
         setContentView(activityEventDetailBinding.root)
@@ -21,7 +39,6 @@ class EventDetailActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = name
 
-
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
@@ -31,8 +48,19 @@ class EventDetailActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        onBackPressedDispatcher.onBackPressed()
-        return true
+        val isFromNotification = intent.getBooleanExtra("EXTRA_FROM_NOTIFICATION", false)
+
+        if (isFromNotification) {
+            val intent = Intent(this, MainNavigation::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            }
+            startActivity(intent)
+            finish()
+            return true
+        } else {
+            onBackPressedDispatcher.onBackPressed()
+            return true
+        }
     }
 
     companion object {
